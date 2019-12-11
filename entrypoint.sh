b5d98516ec3f13735d4be81e25b56f2596154e1f
Patawee Boonsongsri <psgolf16@gmail.com>
2019-12-11 13:47:04 +0700

    add permission

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,10 @@ create_file() {
   echo $1 |base64 -d > $2
 }
 
+add_permission() {
+  chmod +x $1
+}
+
 connect_vpn() {
 openvpn --config $1 --daemon
 
@@ -45,14 +49,20 @@ fi
 
 if [[ "$INPUT_SECRET" != "" ]]; then
     create_file $INPUT_SECRET secret.txt
+    add_permission secret.txt
 fi
 
 if [[ "$INPUT_TLS_KEY" != "" ]]; then
     create_file $INPUT_TLS_KEY tls.key
+    add_permission tls.key
 fi
 
 create_file $CA_CRT ca.crt
 create_file $USER_CRT user.crt
 create_file $USER_KEY user.key
+
+add_permission ca.crt
+add_permission user.crt
+add_permission user.key
 
 connect_vpn $INPUT_FILE_OVPN $INPUT_PING_URL
